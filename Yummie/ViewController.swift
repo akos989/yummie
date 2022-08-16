beb49de9e01cbc4e0e5b1a30fc94fcf52d85eb4e
akos989 <morvaiakos1998@gmail.com>
2022-08-16 11:04:01 +0200

    Initial Commit

---
+++ Yummie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummie
+//
+//  Created by Ákos Morvai on 2022. 08. 16..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
